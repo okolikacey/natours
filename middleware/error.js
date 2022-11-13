7e14a094e81d0f18188654bb572ec8e35adb7f18
Kacey Nnaemeka <kacey.nnaemeka@flutterwavego.com>
2022-11-13 02:01:22 +0100

    Implemented error handling

--- middleware/error.js
+++ middleware/error.js
@@ -1,9 +1,57 @@
-const winston = require('winston'); //for error logging
+const AppError = require('../utils/appError');
+const logger = require('../utils/logging');
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.keyValue.name;
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((error) => error.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  logger.error(err);
+  logger.error(err.stack);
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+    error: err,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  //Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    //Programming or other unknown error: don't leak error details
+  } else {
+    // logger.error(err);
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+};
 
 module.exports = function (err, req, res, next) {
-  winston.error(err.message, err);
-  res.status(500).json({
-    status: 'failed',
-    message: err.message,
-  });
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error._message.includes('validation failed')) error = handleValidationErrorDB(error);
+    sendErrorProd(error, res);
+  }
 };
